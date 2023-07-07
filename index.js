98fb83dd596a779d777c3ed763e5e5f2dc6fd1c9
saimsaleem <engssaleem@gmail.com>
2023-07-08 03:03:27 +0500

    v1.1

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-function addRow() {
-    var tableRow = document.getElementById("table");
-    var day =  document.getElementById("day").value;
-    var row = document.createElement("tr");
-    var cell1 = document.createElement("td");
-     cell1.innerHTML = day;
 
-    cell1.appendChild(btn);
-     row.setAttribute('id',day);
-     row.appendChild(cell1);
-     tableRow.appendChild(row);
-
-     row.appendChild(btn);
-     console.log(document.getElementById(day));
+document.addEventListener('DOMContentLoaded', function() {
+    var images = document.getElementsByTagName('img');
+    var imageCount = images.length;
+    var loadedImages = 0;
+    Array.from(images).forEach(function(image) {
+      if (image.complete) {
+        loadedImages++;
+      } else {
+        image.addEventListener('load', function() {
+          loadedImages++;
+          if (loadedImages === imageCount) {
+            showPage();
+          }
+        });
+        image.addEventListener('error', function() {
+          loadedImages++;
+          if (loadedImages === imageCount) {
+            showPage();
+          }
+        });
+      }
+    });
+  
+    if (loadedImages === imageCount) {
+      showPage();
     }
-
-function addHead(){
-    var tablehead = document.getElementById("tablehead");
-    var time =  document.getElementById("time").value;
-    var cell1 = document.createElement("th");
-     cell1.innerHTML = time;
-     cell1.setAttribute('id',time);
-     tablehead.appendChild(cell1);
-     console.log(document.getElementById(time));
-}
-
-function addCourse(){
-    var column =  document.getElementById("column").value;
-    var row =  document.getElementById("row").value;
-    var course =  document.getElementById("course").value;
-    var day = document.getElementById(row);
-    var time =  document.getElementById(column);
-    var cell1 = document.createElement("td");
-     cell1.innerHTML = course;
-     cell1.innerHTML =  "<button id='btn'>course</button>";
-     cell1.setAttribute('id',course);
-     day.appendChild(cell1);
-     console.log(document.getElementById(course));
-}
+  
+    function showPage() {
+        document.getElementById('animation-container').style.display = 'flex';
+        document.getElementById('image-container').style.display = 'flex';
+        document.getElementById('content-container2').style.display = 'block';
+        document.getElementById('content-container').style.display = 'block';
+       
+    }
+  });
+  
