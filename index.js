eaee73258a14e5e6920c279d0e0b3f071c0854a7
saimsaleem <engssaleem@gmail.com>
2023-08-17 15:18:44 +0500

    f

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-console.log("Loading")
-window.addEventListener('load', function() {
-    console.log("Ready")
-    // All images and other resources have loaded
-    const loadingScreen = document.querySelector('.loading-screen');
-    const mainContent = document.querySelector('.main-content');
-  
-    loadingScreen.style.display = 'none'; // Hide loading screen
-    mainContent.style.display = 'block';   // Display main content
-
     const button = document.getElementById('servicebtn');
 
     button.addEventListener('click', function() {
@@ -17,7 +7,3 @@ window.addEventListener('load', function() {
       // Open the link in a new tab
       window.open(linkUrl, '_blank');
     });
-
-  });
-
-  
